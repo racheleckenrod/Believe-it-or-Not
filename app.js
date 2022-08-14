9db683e49ae527d493fb00a76d2bb887218e55a8
racheleckenrod <rachel@racheleckenrod.com>
2022-08-13 20:15:15 -0500

    completed app

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
-// const MongoStore = require('connect-mongo')(session)
+// mayanwolfe's updates (session) wasn't working
+const MongoStore = require('connect-mongo')//(session)
 const connectDB = require('./config/db')
-// const { default: mongoose } = require('mongoose')
+
 
 // load config
 dotenv.config({ path: './config/config.env'})
@@ -20,14 +22,45 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+// method-override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 // Logging in console
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 // Handlebars - using '.hbs' instead of '.handlebars' (?) 
 // needed to use 'exphbs.engine'  instead of just 'exphbs' like it showed in the video
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine(
+    '.hbs', 
+    exphbs.engine({
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select,
+        },
+        defaultLayout: 'main',
+         extname: '.hbs'
+    })
+)
+
 app.set('view engine', '.hbs')
 
 // express-session middleware - needs to be above passport middleware
@@ -36,12 +69,21 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    // store: new MongoStore({mongooseConnection: mongoose.connection })
+    // Mayanwolfe's UPDATE MongoStore is updated
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
 }))
 
 // Set passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req,res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -51,6 +93,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 
 const PORT = process.env.PORT || 3000
