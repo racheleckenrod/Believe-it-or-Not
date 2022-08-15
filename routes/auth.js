0ed2e873ee2c9c5ead4a8ed9ef86877c3f41b7cd
racheleckenrod <rachel@racheleckenrod.com>
2022-08-14 22:51:49 -0500

    logout attempt

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get(
 router.get('/logout', (req,res,next) => {
     req.logout(function(err) {
         if (err) {return next(err)}
-        res.redirect('https://accounts.google.com/o/oauth2/revoke?token="+ACCESS_TOKEN')
+        res.redirect("https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=/")
     })
 })
 
